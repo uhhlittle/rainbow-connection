94e6ef0cc9af316f820aab1becdc14c220fe9d67
ALittle <alittle@skylinenet.net>
2018-08-27 14:38:26 -0400

    adding color-menu

---
+++ src/app/color-menu/color-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-menu',
+  templateUrl: './color-menu.component.html',
+  styleUrls: ['./color-menu.component.css']
+})
+export class ColorMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
