b188234fb1121b672519be869f3c4ff2d6e1e520
ALittle <allysonjlittle@gmail.com>
2018-09-05 17:48:59 -0400

    changing color-menu to app-color-menu to follow naming conventions, psuedo-coding giphy api call

--- src/app/color-menu/color-menu.component.ts
+++ src/app/color-menu/color-menu.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { timeout } from '../../../node_modules/@types/q';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
-  selector: 'color-menu',
+  selector: 'app-color-menu',
   templateUrl: './color-menu.component.html',
   styleUrls: ['./color-menu.component.css']
 })
@@ -21,7 +24,26 @@ export class ColorMenuComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.getGif('red');
   }
 
+  getGif(parameter: string): void {
+    const gif = $.get(`http://api.giphy.com/v1/gifs/search?q=${parameter}&api_key=YOUR_API_KEY&limit=5`);
+    gif.done(function (data) { console.log("success got data", data); });
+  }
+
+  getColorTV(color: string, continuous: boolean) {
+    // cancel intervals
+    if (!color) {
+      return this.getRainbowTV();
+    }
+    if (!continuous) {
+      return this.getGif(color);
+    }
+    // set up an interval that sets this.gif with color parameter every 10 seconds.
+  }
+
+  getRainbowTV(): void {
+   // start interval that cycles through colors array every 10 seconds.
+  }
 }
